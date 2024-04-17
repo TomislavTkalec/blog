0f69339499e4e322b1ea03d5d464a4839c6b2262
TomislavTkalec <tomislav.tkalec@appliment.eu>
2024-04-17 12:34:14 +0200

    add: blade components

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -46,7 +46,13 @@ class Post
 
     public static function find($slug)
     {
-        return static::all()->firstWhere('slug', $slug);
+        $post = static::all()->firstWhere('slug', $slug);
+
+        if(!$post){
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
     }
 
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,18 +1,14 @@
-<!DOCTYPE html>
-
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
-
-<body>
+<x-layout>
+    
     <article>
-        <h1><?= $post->title; ?></h1>
+        <h1>{{$post->title}}</h1>
 
         <div>
-            <?= $post->body; ?>
+            {!! $post->body !!}
         </div>
 
     </article>
     <a href="/">Go back</a>
 
-</body>
+</x-layout>
 
